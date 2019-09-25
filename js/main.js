fd02bfc4baff067ce0ad3db99649be310b7aa70c
sross9845 <sean.ross.985@gmail.com>
2019-09-25 16:39:52 -0700

    Kanye's Best

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("👀")
+function fetchThatStuffYo(){
+    fetch('https://api.kanye.rest') 
+    .then(function(kanyesBest) {
+        return kanyesBest.json();
+      })
+      .then(function(jsonData) {
+        let kanye = jsonData.quote
+        let best = document.getElementById("kanyesbest")
+        best.textContent = '"' +kanye+'"' + " - Kanye West"
+        console.log(kanye)
+      });
+}
+document.getElementById("wisdomgiver").addEventListener("click",fetchThatStuffYo)
